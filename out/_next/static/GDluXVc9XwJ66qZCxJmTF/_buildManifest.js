a04b7e55968e355eae550de7f72db56c13df9c69
Tyrel Succar <tyrelsuccar@hotmail.com>
2022-09-15 13:34:54 -0400

    back to latest version & html config

--- out/_next/static/GDluXVc9XwJ66qZCxJmTF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7c9e31c2156353a4.css","static/chunks/pages/index-e9f49e190de52ca2.js"],"/_error":["static/chunks/pages/_error-0509152792d2b207.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
