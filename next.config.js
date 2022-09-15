82dbbdc3538ec8c767a0f0f2950e2efbd848a3fa
Tyrel Succar <tyrelsuccar@Tyrels-MacBook-Pro.local>
2022-09-15 08:35:39 -0400

    again updated

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: { unoptimized: true }  
+  images: { unoptimized: true },
+  assetPrefix: "./"  
 }
 
 module.exports = nextConfig
