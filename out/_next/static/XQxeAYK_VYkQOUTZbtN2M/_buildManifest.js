1fa4e127fce3caa555d1212037f18bf8fe6b3fb4
Tyrel Succar <tyrelsuccar@hotmail.com>
2022-09-15 12:51:36 -0400

    new initial commit

--- out/_next/static/XQxeAYK_VYkQOUTZbtN2M/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7c9e31c2156353a4.css","static/chunks/pages/index-f347cf9a71b2fb58.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
